fad893683518a893295bd885a51c98ba79b39bf3
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2020-09-27 17:10:27 +0530

    setup env in heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBeR1o-EVFUEhej04PSwY78wPGWXS7duj8",
-    authDomain: "expensify-34ea0.firebaseapp.com",
-    databaseURL: "https://expensify-34ea0.firebaseio.com",
-    projectId: "expensify-34ea0",
-    storageBucket: "expensify-34ea0.appspot.com",
-    messagingSenderId: "519194978661",
-    appId: "1:519194978661:web:d350405ba0454ce71c198c",
-    measurementId: "G-SZHXS6G20Y"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DotEnv = require('dotenv');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+DotEnv.config({path: '.env'});
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -37,7 +43,19 @@ module.exports = (env) => {
           }],
       }]
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE': JSON.stringify(process.env.FIREBASE_DATABASE),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+      })
+    ],
     devtool: isProduction ? 'source-map' : 'inline : source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
